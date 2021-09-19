341546f8d260321b8fc3c94dbf8d40e863896ab9
Plastic_X_Plastic <80278929+PlasticxPlastic@users.noreply.github.com>
2021-09-19 19:31:30 +0700

    Update Problem6_2.cpp

--- ProfundWeekly/Problem6_2.cpp
+++ ProfundWeekly/Problem6_2.cpp
@@ -3,27 +3,36 @@ int main()
 {
     int num,i=1,T,H,j=1,digit;
     scanf("%d",&num);
+    
     while(i*i < num) // find largest pow2 that below than num
     {
-        i = i + 1;
+        i++;
     }
-    i = i-1;
-    printf("%d.",i); // printตัวหน้าทศนิยม
-
-    T = 2 * i;//ตัวตั้ง
-    H = num - (i*i); // ตัวหาร
-    while( j <= 5)//เอา5ตำแหน่ง
+    if(i*i == num)
     {
-        H = H*100;
-        digit = 0;
-        while((T*10+digit) * digit<H)
-        {
-            digit = digit + 1;
-        }
-        digit = digit-1;
-        printf("%d",digit);
-        H = H-((T*10+digit)*digit);
-        T = T * 10 + 2 * digit;
-        j = j + 1;
+        printf("%d",i);
     }
+    else
+    {
+        i = i-1;
+        printf("%d.",i); // printตัวหน้าทศนิยม
+
+        T = 2 * i;//ตัวตั้ง
+        H = num - (i*i); // ตัวหาร
+        while( j <= 5)//เอา5ตำแหน่ง
+        {
+            H = H*100;
+            digit = 0;
+            while((T*10+digit) * digit<H)
+            {
+                digit = digit + 1;
+            }
+            digit = digit-1;
+            printf("%d",digit);
+            H = H-((T*10+digit)*digit);
+            T = T * 10 + 2 * digit;
+            j = j + 1;
+        }
+    }
+    
 }
