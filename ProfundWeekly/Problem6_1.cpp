4eaf1e5441c91d1ba2cd48e96702377b5c735591
Plastic_X_Plastic <80278929+PlasticxPlastic@users.noreply.github.com>
2021-09-19 19:28:23 +0700

    Create Problem6_1.cpp

---
+++ ProfundWeekly/Problem6_1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int number;
+
+    float x, sqrt;
+    scanf("%d", &number);
+    sqrt = number / 2;
+    x = 0;
+    while(sqrt != x){
+        x = sqrt;
+        sqrt = ( number/x + x) / 2;
+    }
+
+    printf("%.5f",sqrt);
+}
