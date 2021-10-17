32e22be2a4493c243ef1c1444c24c840294772f0
Plastic_X_Plastic <80278929+PlasticxPlastic@users.noreply.github.com>
2021-10-17 22:50:18 +0700

    Create Problem9_1.cpp

---
+++ ProfundWeekly/Problem9_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+#define SIZE 100
+int main()
+
+{
+    int array[SIZE];
+    int i, max, min, size;
+    cout<<"Enter size of the array: ";
+    cin>>size;
+    cout<<"\n Enter "<<size <<" elements in the array: ";
+    for(i=0; i<size; i++)
+        cin>>array[i];
+    max = array[0];
+    min = array[0];
+    for(i=1; i<size; i++)
+    {
+        if(array[i] > max)
+            max = array[i];
+        if(array[i] < min)
+            min = array[i];
+    }
+    cout<<"\nMaximum=" << max << "\n";
+    cout<<"Minimum=" << min;
+    return 0;
+}
